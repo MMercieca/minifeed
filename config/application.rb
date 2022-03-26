09215b613a77c6bc716e927ae58a7ecb8837fb19
Matthew Mercieca <mercieca@gmail.com>
2022-03-25 22:08:21 -0400

    Trying to deploy

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Minicast
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.css_compressor = nil
   end
 end
