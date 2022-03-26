28ceee791a40d6d479cf4a9088a3f7c1e459a443
Matthew Mercieca <mercieca@gmail.com>
2022-03-26 13:15:58 -0400

    Attempting Google Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable if Rails.env.development?
-  devise :rememberable, :omniauthable, omniauth_providers: [:google_oauth2]
+  # devise :database_authenticatable if Rails.env.development?
+  devise :database_authenticatable, :rememberable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :main_feeds
 end
