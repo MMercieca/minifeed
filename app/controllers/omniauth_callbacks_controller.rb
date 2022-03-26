1706ada3f42071b90616fe7327b18a4c7395027f
Matthew Mercieca <mercieca@gmail.com>
2022-03-26 13:13:39 -0400

    Attempting Google Auth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # replace with your authenticate method
-  skip_before_action :authenticate_user!
+  # skip_before_action :authenticate_user!
 
   def google_oauth2
     auth = request.env["omniauth.auth"]
