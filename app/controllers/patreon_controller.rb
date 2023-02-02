f68c34ea345d451569934d9abc99cf372d9295ac
Matt Mercieca <mercieca@gmail.com>
2023-02-01 20:17:32 -0500

    WIP

--- app/controllers/patreon_controller.rb
+++ app/controllers/patreon_controller.rb
@@ -6,19 +6,24 @@ class PatreonController < ApplicationController
     access_token = tokens['access_token']
 
     api_client = Patreon::API.new(access_token)
-    user = api_client.fetch_user()
-    campaign = api_client.fetch_campaign_and_patrons()
-    binding.pry
-    puts
-    # user_response uses [json-api-vanilla](https://github.com/trainline/json-api-vanilla) for easy usage
-    render plain: api_client.fetch_user().data
+    email = api_client.fetch_user().data.email
+
+    # TODOMPM - make this work with google
+    user = User.find_by(email: email)
+    if !user
+      user = User.create(email: email, provider: 'patreon')
+    end
+
+    sign_in(:user, user)
+    redirect_to after_sign_in_path_for(user)
   end
 
   def patreon_launch
     login_url = "https://www.patreon.com/oauth2/authorize" \
       "?response_type=code" \
       "&client_id=#{ENV.fetch('PATREON_CLIENT_ID')}" \
-      "&redirect_uri=http://localhost:3000/patreon_callback"
+      "&redirect_uri=http://localhost:3000/patreon_callback" \
+      "&scopes=identity.email,identity.memberships"
     redirect_to login_url, allow_other_host: true
   end
 end
