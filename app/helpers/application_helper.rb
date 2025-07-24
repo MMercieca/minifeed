9d09318b00b0ceb9853944f2490bfbcf99e27b24
Matt Mercieca <mercieca@gmail.com>
2025-07-23 20:34:28 -0400

    Making images for RSS feeds on S3 public by default
    The RSS feed is public and signed S3 URLs time out.  This makes the image privacy settings match the RSS feed privacy settings.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # The RSS feeds that minicast serves are public.  Signed S3 URLs time out, so let's construct
+  # a URL that will be public as long as the feed is.
+  def public_url(image)
+    if Rails.env.production?
+      "#{ENV["PUBLIC_S3_URL"]}/#{image.blob.key}"
+    else
+      image.url
+    end
+  end
 end
