5008ff9ca983581235c193e7d40180e550578e0e
Matt Mercieca <mercieca@gmail.com>
2025-08-15 20:10:23 -0400

    Adding option to create feeds that include all episodes and just scrub for Android auto.

--- app/controllers/main_feeds_controller.rb
+++ app/controllers/main_feeds_controller.rb
@@ -25,6 +25,16 @@ class MainFeedsController < ApplicationController
     main_feed.fetch
     main_feed.reload
     
+    if params[:android_auto]
+      mini_feed = MiniFeed.create!(
+        main_feed: main_feed,
+        name: main_feed.name,
+        ensure_android_auto_compatability: true,
+        select_all_episodes: true
+      )
+      mini_feed.image.attach(main_feed.image.blob)
+    end
+
     redirect_to known_feed_url(identifier: main_feed.identifier)
   end
 
